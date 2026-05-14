83025bf51ac46e8cc6d2cc64e3d8f6fa4de925fc
Umar Salim Pathan <78787284+Up25-pathan@users.noreply.github.com>
2026-05-14 09:59:29 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true, // Use SSL
+  family: 4,    // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
